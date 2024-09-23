1a2ef921272c7257d708e2f79dabbb9bf4659ee4
Asma Akbar <masoomlarki297@gmail.com>
2024-09-23 11:10:20 +0500

    Push my 2nd assignment 2 components about and header

---
+++ src/app/Components/About.tsx
@@ -0,0 +1,11 @@
+function About(){
+    return(
+        <div>
+            <center>
+                <br />
+                <h1>This is a About</h1>
+            </center>
+        </div>
+    )
+}
+export default About
---
+++ src/app/Components/Header.tsx
@@ -0,0 +1,11 @@
+function Header (){
+    return (
+        <div>
+            <center>
+                <br />
+                This is a Header
+            </center>
+        </div>
+    )
+}
+export default Header
